4808ff5e14f82a664454761d8fa8893ca1790367
zhoumushui <zhoumushui@gmail.com>
2017-04-18 14:47:12 +0800

    [ZMS]Temp commit.

--- src/main/java/com/zhoumushui/zygoteelastichome/MainActivity.java
+++ src/main/java/com/zhoumushui/zygoteelastichome/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     BuildLayerLinearLayout buildLayerLinearLayout;
     public TextView textView;
 
+    private static final int ITEM_SIZE = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int width = (displayMetrics.widthPixels - 60) / 3;
+        int width = (displayMetrics.widthPixels - 60) / ITEM_SIZE;
         int height = (int) (width / 340f * 600);
         int viewwidth = 30;
 
@@ -51,16 +53,16 @@ public class MainActivity extends AppCompatActivity {
 
         CardView cardView1 = new CardView(this);
         cardView1.setLayoutParams(new LinearLayout.LayoutParams(width, height));
-        //buildLayerLinearLayout.addView(cardView1);
+//        buildLayerLinearLayout.addView(cardView1);
         TextView textView = new TextView(this);
-        textView.setText("hello");
+        textView.setText("ADAS");
         textView.setGravity(Gravity.CENTER);
         cardView1.addView(textView);
 
         cardView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                t.setText("click");
+                t.setText("click ADAS");
             }
         });
 
@@ -68,58 +70,80 @@ public class MainActivity extends AppCompatActivity {
         imageView1.setLayoutParams(new LinearLayout.LayoutParams(width, height));
         buildLayerLinearLayout.addView(imageView1);
         imageView1.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
-                R.drawable.pic1, width, height));
+                R.drawable.icon_music_light, width, height));
         imageView1.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
 
         BezelImageView imageView2 = new BezelImageView(this);
         imageView2.setLayoutParams(new LinearLayout.LayoutParams(width, height));
         buildLayerLinearLayout.addView(imageView2);
         imageView2.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
-                R.drawable.pic2, width, height));
+                R.drawable.icon_navigation_light, width, height));
         imageView2.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
 
         BezelImageView imageView3 = new BezelImageView(this);
         imageView3.setLayoutParams(new LinearLayout.LayoutParams(width, height));
         buildLayerLinearLayout.addView(imageView3);
         imageView3.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
-                R.drawable.pic3, width, height));
+                R.drawable.icon_record_light, width, height));
         imageView3.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
 
         BezelImageView imageView4 = new BezelImageView(this);
         imageView4.setLayoutParams(new LinearLayout.LayoutParams(width, height));
         buildLayerLinearLayout.addView(imageView4);
         imageView4.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
-                R.drawable.pic4, width, height));
+                R.drawable.icon_map_light, width, height));
         imageView4.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
 
         BezelImageView imageView5 = new BezelImageView(this);
         imageView5.setLayoutParams(new LinearLayout.LayoutParams(width, height));
         buildLayerLinearLayout.addView(imageView5);
         imageView5.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
-                R.drawable.pic5, width, height));
+                R.drawable.icon_filemanager_light, width, height));
         imageView5.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
 
         BezelImageView imageView6 = new BezelImageView(this);
         imageView6.setLayoutParams(new LinearLayout.LayoutParams(width, height));
         buildLayerLinearLayout.addView(imageView6);
         imageView6.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
-                R.drawable.pic6, width, height));
+                R.drawable.icon_fmtransmit_light, width, height));
         imageView6.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
 
         BezelImageView imageView7 = new BezelImageView(this);
         imageView7.setLayoutParams(new LinearLayout.LayoutParams(width, height));
         buildLayerLinearLayout.addView(imageView7);
         imageView7.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
-                R.drawable.pic7, width, height));
+                R.drawable.icon_wechat_light, width, height));
         imageView7.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
+
+        BezelImageView imageView8 = new BezelImageView(this);
+        imageView8.setLayoutParams(new LinearLayout.LayoutParams(width, height));
+        buildLayerLinearLayout.addView(imageView8);
+        imageView8.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
+                R.drawable.icon_edog_light, width, height));
+        imageView8.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
+
+        BezelImageView imageView9 = new BezelImageView(this);
+        imageView9.setLayoutParams(new LinearLayout.LayoutParams(width, height));
+        buildLayerLinearLayout.addView(imageView9);
+        imageView9.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
+                R.drawable.icon_fmonline_light, width, height));
+        imageView9.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
+
+        BezelImageView imageView10 = new BezelImageView(this);
+        imageView10.setLayoutParams(new LinearLayout.LayoutParams(width, height));
+        buildLayerLinearLayout.addView(imageView10);
+        imageView10.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
+                R.drawable.icon_setting_light, width, height));
+        imageView10.setMaskDrawable(getResources().getDrawable(R.drawable.roundrect));
 
         view = new View(this);
         view.setLayoutParams(new LinearLayout.LayoutParams(30, 0));
         buildLayerLinearLayout.addView(view);
 
         elasticScrollView.setChildViews(new View[]{
-                cardView1, imageView1, imageView2, imageView3,
-                imageView4, imageView5, imageView6, imageView7});
+               /* cardView1, */imageView1, imageView2, imageView3,
+                imageView4, imageView5, imageView6, imageView7,
+                imageView8, imageView9, imageView10});
 
         elasticScrollView.setScrollChangeListener(new ElasticScrollView.OnScrollChangeListener() {
             @Override
